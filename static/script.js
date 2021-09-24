47c2d0804df372515c741b952d707404999807bf
Samael Melo <samaelmelo.dev@gmail.com>
2021-09-23 21:06:08 -0400

    estrutura completa do projeto

--- static/script.js
+++ static/script.js
@@ -2,11 +2,13 @@
 
 const menuMobile = document.querySelector('.menu-mobile')
 const body = document.querySelector('body')
+const footer = document.querySelector('#footer')
 
 menuMobile.addEventListener('click', () => {
   menuMobile.classList.toggle('active')
   body.classList.toggle('menu-nav-active')
-})
+  footer.classList.toggle('wid')
+});
 
 // fechar o menu quando clicar em alguma item e muda o icone para list
 const navItem = document.querySelectorAll('.nav-item')
@@ -15,8 +17,22 @@ navItem.forEach(item => item.addEventListener('click', () => {
   if(body.classList.contains('menu-nav-active')){
     body.classList.remove('menu-nav-active')
     menuMobile.classList.toggle('active')
+    footer.classList.toggle('wid')
   }
-}))
+}));
+
+// mostrar link ativo no dashboard
+const links = document.querySelectorAll('.nav-item a')
+  
+links.forEach( link => link.addEventListener('click', (event) => {
+  const elementClass = event.target.className === 'nav-link active'
+    for(element of links){
+      element.classList.remove('active')
+    }
+
+    elementClass ?  link.classList.remove('active') : link.classList.add('active')
+}));
+
 
 
 // animar todos os itens da tela que tiverem o atributo data-anime
@@ -33,15 +49,15 @@ const animeScroll= () =>{
       element.classList.remove('animate')
     }
   
-  })
+  });
   
-}
+};
 
 animeScroll()
 
 window.addEventListener('scroll', () => {
   animeScroll()
-})
+});
 
 
 // ativar o carregamento no botao enviar formilario para
@@ -53,10 +69,10 @@ const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
 btnEnviar.addEventListener('click', ()=>{
   btnEnviarLoader.style.display = 'block'
   btnEnviar.style.display = 'none'
-})
+});
 
 // tirar a mensagem depois de 5 segundos
 
 setTimeout( () => {
   document.querySelector('#alerta').style.display = 'none'
-},4000)
+},4000);
